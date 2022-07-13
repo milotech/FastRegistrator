0910d2330e07cdfaa40fd97ee10b95f52913076b
Danil <kr.dan1995@gmail.com>
2022-07-13 12:33:53 +0300

    Added controllers

---
+++ FastRegistrator.API/Controllers/CheckUserByMobileController.cs
@@ -0,0 +1,14 @@
+﻿using FastRegistrator.ApplicationCore.Commands.CheckUserByMobilePhone;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FastRegistrator.API.Controllers
+{
+    public class CheckUserByMobilePhoneController : ApiControllerBase
+    {
+        [HttpPost]
+        public async Task<ActionResult<bool>> Check(CheckUserByMobilePhoneCommand command) 
+        {
+            return await Mediator.Send(command);
+        }
+    }
+}
