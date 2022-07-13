a326c101eb586832d49eee37d9965b14584dc6fc
Дмитрий <developer240997@gmail.com>
2022-07-13 08:37:23 +0300

    setup_mediatr

---
+++ FastRegistrator.Application/Behaviours/ExceptionBehaviour.cs
@@ -0,0 +1,29 @@
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace FastRegistrator.ApplicationCore.Behaviours
+{
+    public class ExceptionBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> where TRequest : notnull
+    {
+        private readonly ILogger<TRequest> _logger;
+
+        public ExceptionBehaviour(ILogger<TRequest> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
+        {
+            try
+            {
+                return await next();
+            }
+            catch (Exception ex)
+            {
+                var requestName = typeof(TRequest).Name;
+                _logger.LogError(ex, "CleanArchitecture Request: Unhandled Exception for Request {Name} {@Request}", requestName, request);
+                throw;
+            }
+        }
+    }
+}
---
+++ FastRegistrator.Application/ConfigureServices.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace FastRegistrator.ApplicationCore
+{
+    public static class ConfigureServices
+    {
+        public static IServiceCollection AddAplicationServices(this IServiceCollection services)
+        {
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+            return services;
+        }
+    }
+}
