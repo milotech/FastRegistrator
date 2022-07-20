7b83ecc6b7181987620712aa997f31340d2e197c
Danil <kr.dan1995@gmail.com>
2022-07-20 13:48:12 +0300

    Fixed Person Status

--- FastRegistrator.Application/Domain/Enums/PersonStatus.cs
+++ FastRegistrator.Application/Domain/Enums/PersonStatus.cs
@@ -2,14 +2,25 @@
 {
     public enum PersonStatus
     {
+        // Не подтверждена учетная запись ЕСИА
         ESIANotApproved,
+        // Подтверждена учетная запись ЕСИА
         ESIAApproved,
+        // Отказ клиента от регистрации
         PersonRejected,
-        SuccessRegistration = 0,
-        FailedRegistration = 1,
-        UserAlreadyExsist = 2,
+        // Потенциальный клиент – Заполнил заявление
+        ClientFilledApplication,
+        // Потенциальный клиент – Отправлен на проверку
         PrizmaCheckInProgress,
+        // Потенциальный клиент – Проверку не прошел
         PrizmaCheckRejected,
+        // Потенциальный клиент – Проверен
         PrizmaCheckSuccessful,
+        // Потенциальный клиент – Готов к регистрации в учетной системе
+        ClientReadyForRegistration,
+        // Клиент – Счет открыт
+        AccountOpened,
+        // Клиент – Счет закрыт
+        AccountClosed
     }
 }
