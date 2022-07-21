1ff0f633ca4293c892fcce49412712a5205951a1
Danil <kr.dan1995@gmail.com>
2022-07-21 17:55:04 +0300

    Improved structure of entites

--- FastRegistrator.Application/Domain/Entities/BankDetail.cs
+++ FastRegistrator.Application/Domain/Entities/BankDetail.cs
@@ -2,14 +2,16 @@
 {
     public class BankDetail
     {
-        public string CheckingAccount { get; private set; } = null!;
-        public string Bic { get; private set; } = null!;
+        public string Name { get; private set; }
+        public string Account { get; private set; } = null!;
+        public string Bik { get; private set; } = null!;
         public string Currency { get; private set; } = null!;
 
-        public BankDetail(string checkingAccount, string bic, string currency)
+        public BankDetail(string name, string account, string bik, string currency)
         {
-            CheckingAccount = checkingAccount;
-            Bic = bic;
+            Name = name;
+            Account = account;
+            Bik = bik;
             Currency = currency;
         }
     }
--- FastRegistrator.Application/Domain/Entities/PersonFormData.cs
+++ FastRegistrator.Application/Domain/Entities/PersonFormData.cs
@@ -1,45 +1,29 @@
-﻿namespace FastRegistrator.ApplicationCore.Domain.Entities
+﻿using FastRegistrator.ApplicationCore.Domain.ValueObjects;
+
+namespace FastRegistrator.ApplicationCore.Domain.Entities
 {
-    public class PersonFormData
+    public class PersonFormData : BaseEntity
     {
         private List<BankDetail> _bankDetails = new();
-        public bool IsUserConsent { get; }
         public IReadOnlyCollection<BankDetail> BankDetails => _bankDetails;
-        public bool Sex { get; private set; }
-        public string SourceOriginFunds { get; private set; } = null!;
-        public string PlaceOfWork { get; private set; } = null!;
+        public bool FinanceAgreement { get; }
+        public string Sex { get; private set; } = null!;
+        public string MoneySource { get; private set; } = null!;
+        public string WorkPlace { get; private set; } = null!;
         public string JobTitle { get; private set; } = null!;
         public string PlannedOperations { get; private set; } = null!;
-        public string PlannedForInvestmentAmountOfFunds { get; private set; } = null!;
+        public string PlannedForInvestmentMoney { get; private set; } = null!;
         public bool IsPresenceOfBeneficiary { get; private set; }
-        public bool IsPresencePoliticallyExposedPersonStatus { get; private set; }
-        public DateTime BirthDate { get; private set; }
-        public string PlaceOfBirth { get; private set; } = null!;
-        public string Citizenship { get; private set; } = null!;
-        public string? IdentityDocument { get; private set; }
-        public Passport? Passport { get; private set; }
-        public string ResidenceAddress { get; private set; } = null!;
-        public string ActualResidenceAddress { get; private set; } = null!;
-        public string Phone { get; private set; } = null!;
-        public string Fax { get; private set; } = null!;
-        public string Email { get; private set; } = null!;
-        public bool IsTaxResidentOfRF { get; private set; }
+        public bool IsPoliticallyExposedPerson { get; private set; }
+        public PrivateData? PrivateData { get; private set; }
+        public bool IsTaxResidentRussia { get; private set; }
         public string Inn { get; private set; } = null!;
-        public bool IsTaxResidentOfAnotherCountry { get; private set; }
+        public bool IsTaxResidentAnotherCountry { get; private set; }
         public string? FullName { get; private set; }
         public string? CountryOfTaxResidence { get; private set; }
         public string? Tin { get; private set; }
         public string? ReasonOfAbsenceTin { get; private set; }
         public string? OffshoreDetail { get; private set;}
-        public bool IsCitizenshipUSA { get; private set; }
-        public bool IsGreenCard { get; private set; }
-        public bool IsLongTermStayUSA { get; private set; }
-        public bool IsBornInUSAAndRefusalCitizenship { get; private set; }
-        public bool IsResidentialInUSA { get; private set; }
-        public bool IsPhoneNumberInUSA { get; private set; }
-        public bool IsTaxResidentInUSA { get; private set; }
-        public string? SSN { get; private set; }
-        public string? ITIN { get; private set; }
-        public string? FullNameIssuedByUSAAuthorities { get; private set; }
+        public USASectionDetails UsaSectionDetails { get; private set; }
     }
 }
