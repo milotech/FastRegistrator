e6e09651095277659696d53b5a4259d0ae581862
Дмитрий <dimals97@hotmail.com>
2022-07-22 15:30:04 +0300

    added prizma servcie and models

---
+++ FastRegistrator.Infrastructure/DTOs/PrizmaServiceDTOs/PersonCheckCommonResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace FastRegistrator.Infrastructure.DTOs.PrizmaServiceDTOs
+{
+    public class PersonCheckCommonResponse
+    {
+        public PersonCheckResult? PersonCheckResult { get; set; }
+        public ErrorResponse? ErrorResponse { get; set; }
+    }
+}
