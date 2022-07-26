983759c46aea6f53fdc420610f93dbce30dfd642
Danil <kr.dan1995@gmail.com>
2022-07-26 11:31:35 +0300

    Added more unit tests for phone validator

--- FastRegistrator.Tests/Validators/CheckPersonByPhoneValidatorTest.cs
+++ FastRegistrator.Tests/Validators/CheckPersonByPhoneValidatorTest.cs
@@ -27,6 +27,86 @@ namespace FastRegistrator.Tests.Validators
         }
 
         [Fact]
+        [Description("Arrange Command come with plus at beginning of phone number" +
+                     "Act Validate method is called" +
+                     "Assert Validation result is true")]
+        public void Validate_PlusBeforePhoneNumber_PassValidation()
+        {
+            // Arrange
+            var validator = new CheckPersonByPhoneCommandValidator();
+            var command = new CheckPersonByPhoneCommand
+            {
+                PhoneNumber = "+79999999999"
+            };
+
+            // Act
+            var result = validator.TestValidate(command);
+
+            // Assert
+            result.ShouldNotHaveValidationErrorFor(x => x.PhoneNumber);
+        }
+
+        [Fact]
+        [Description("Arrange Command come with parentheses in phone number" +
+                     "Act Validate method is called" +
+                     "Assert Validation result is true")]
+        public void Validate_ParenthesesInPhoneNumber_PassValidation()
+        {
+            // Arrange
+            var validator = new CheckPersonByPhoneCommandValidator();
+            var command = new CheckPersonByPhoneCommand
+            {
+                PhoneNumber = "8(999)9999999"
+            };
+
+            // Act
+            var result = validator.TestValidate(command);
+
+            // Assert
+            result.ShouldNotHaveValidationErrorFor(x => x.PhoneNumber);
+        }
+
+        [Fact]
+        [Description("Arrange Command come with spaces in phone number" +
+                     "Act Validate method is called" +
+                     "Assert Validation result is true")]
+        public void Validate_SpacesInPhoneNumber_PassValidation()
+        {
+            // Arrange
+            var validator = new CheckPersonByPhoneCommandValidator();
+            var command = new CheckPersonByPhoneCommand
+            {
+                PhoneNumber = "8 999 999 99 99"
+            };
+
+            // Act
+            var result = validator.TestValidate(command);
+
+            // Assert
+            result.ShouldNotHaveValidationErrorFor(x => x.PhoneNumber);
+        }
+
+        [Fact]
+        [Description("Arrange Command come with dashes in phone number" +
+                     "Act Validate method is called" +
+                     "Assert Validation result is true")]
+        public void Validate_DashesInPhoneNumber_PassValidation()
+        {
+            // Arrange
+            var validator = new CheckPersonByPhoneCommandValidator();
+            var command = new CheckPersonByPhoneCommand
+            {
+                PhoneNumber = "8-999-999-99-99"
+            };
+
+            // Act
+            var result = validator.TestValidate(command);
+
+            // Assert
+            result.ShouldNotHaveValidationErrorFor(x => x.PhoneNumber);
+        }
+
+        [Fact]
         [Description("Arrange Command come with empty phone number" +
                      "Act Validate method is called" +
                      "Assert Validation result is false and it has appropriate error message")]
@@ -45,6 +125,27 @@ namespace FastRegistrator.Tests.Validators
             // Assert
             result.ShouldHaveValidationErrorFor(x => x.PhoneNumber)
                   .WithErrorMessage(CheckPersonByPhoneCommandValidator.MobilePhoneIsEmpty);
+        }
+
+        [Fact]
+        [Description("Arrange Command come with wrong country code in phone number" +
+                     "Act Validate method is called" +
+                     "Assert Validation result is false and it has appropriate error message")]
+        public void Validate_WrongCountryCode_FalseValidationAndErrorMessage()
+        {
+            // Arrange
+            var validator = new CheckPersonByPhoneCommandValidator();
+            var command = new CheckPersonByPhoneCommand
+            {
+                PhoneNumber = "19999999999"
+            };
+
+            // Act
+            var result = validator.TestValidate(command);
+
+            // Assert
+            result.ShouldHaveValidationErrorFor(x => x.PhoneNumber)
+                  .WithErrorMessage(CheckPersonByPhoneCommandValidator.MobilePhoneHasWrongFormat);
         }
 
         [Fact]
