ad67564250e916550e6608b39dc2454e5c8436a1
Danil <kr.dan1995@gmail.com>
2022-08-02 11:55:03 +0300

    Removed redundant code. And also did some fixes after deleting.

---
+++ FastRegistrator.Application/IntegrationEvents/Events/ESIAApprovedEvent.cs
@@ -0,0 +1,19 @@
+﻿using FastRegistrator.ApplicationCore.Interfaces;
+
+namespace FastRegistrator.ApplicationCore.IntegrationEvents.Events
+{
+    public class ESIAApprovedEvent : IIntegrationEvent
+    {
+        public string PhoneNumber { get; init; } = null!;
+        public string FirstName { get; init; } = null!;
+        public string? MiddleName { get; init; }
+        public string LastName { get; init; } = null!;
+        public string Series { get; init; } = null!;
+        public string Number { get; init; } = null!;
+        public string IssuedBy { get; init; } = null!;
+        public DateTime IssueDate { get; init; }
+        public string IssueId { get; init; } = null!;
+        public string Citizenship { get; init; } = null!;
+        public string Snils { get; init; } = null!;
+    }
+}
