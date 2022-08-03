98ff1a570d888c3aa06ee26493d6b7e62b737ba9
Danil <kr.dan1995@gmail.com>
2022-08-03 14:01:20 +0300

    Fixed PrizmaCheckError entity

--- FastRegistrator.Application/Domain/Entities/PrizmaCheckError.cs
+++ FastRegistrator.Application/Domain/Entities/PrizmaCheckError.cs
@@ -2,8 +2,15 @@
 {
     public class PrizmaCheckError : BaseEntity<int>
     {
-        public string? ErrorMessage { get; private set; }
+        public string ErrorMessage { get; private set; }
+        public int StatusCode { get; private set; }
         public int PrizmaErrorCode { get; private set; }
-        public Dictionary<string, List<string>>? Errors { get; private set; }
+
+        public PrizmaCheckError(string errorMessage, int statusCode, int prizmaErrorCode)
+        {
+            ErrorMessage = errorMessage;
+            StatusCode = statusCode;
+            PrizmaErrorCode = prizmaErrorCode;
+        }
     }
 }
