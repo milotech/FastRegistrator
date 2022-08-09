1bf9021220ef4a61d6e28fa882b1cf234dbc23b5
Danil <kr.dan1995@gmail.com>
2022-08-09 18:57:48 +0300

    Fixed possible NullReferenceException. And changed ICResult to null.

--- FastRegistrator.Application/Queries/GetRegistrationStatusQuery.cs
+++ FastRegistrator.Application/Queries/GetRegistrationStatusQuery.cs
@@ -1,4 +1,5 @@
 ﻿using FastRegistrator.ApplicationCore.Domain.Entities;
+using FastRegistrator.ApplicationCore.Domain.Enums;
 using FastRegistrator.ApplicationCore.DTOs.GetStatusDTOs;
 using FastRegistrator.ApplicationCore.Exceptions;
 using FastRegistrator.ApplicationCore.Interfaces;
@@ -42,13 +43,11 @@ namespace FastRegistrator.ApplicationCore.Queries.GetStatus
 
             var statusHistoryItem = registration.StatusHistory.First();
             
-            var prizmaRejectionReason = statusHistoryItem!.PrizmaCheckResult!.RejectionReasonCode;
-
-            var icResult = new ICResult();
+            var prizmaRejectionReason = statusHistoryItem!.PrizmaCheckResult?.RejectionReasonCode ?? RejectionReason.None;
 
             var error = ConstructError(statusHistoryItem);
 
-            return new RegistrationStatusResponse(registration.Id, registration.Completed, statusHistoryItem.Status, prizmaRejectionReason, icResult, error);
+            return new RegistrationStatusResponse(registration.Id, registration.Completed, statusHistoryItem.Status, prizmaRejectionReason, null, error);
         }
 
         private Error ConstructError(StatusHistoryItem statusHistoryItem) 
