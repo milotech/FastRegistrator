c530e56d1aee2f7b06b8010fd351b28b1f67fcc9
Danil <kr.dan1995@gmail.com>
2022-08-10 17:30:53 +0300

    Added common class for unit test constants.

--- FastRegistrator.Tests/Commands/StartRegistrationCommandHandlerTest.cs
+++ FastRegistrator.Tests/Commands/StartRegistrationCommandHandlerTest.cs
@@ -5,30 +5,20 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using Moq;
 using System.ComponentModel;
+using static FastRegistrator.Tests.Constants;
 
 namespace FastRegistrator.Tests.Commands
 {
     public class StartRegistrationCommandHandlerTest : TestWithDbContext
     {
-        public static readonly Guid GUID = new Guid("00000000-0000-0000-0000-000000000000");
-        public const string PERSON_PHONE_NUMBER = "+79999999999";
-        public const string FIRST_NAME = "Ivan";
-        public const string LAST_NAME = "Ivanov";
-        public const string MIDDLE_NAME = "Ivanovich";
-        public const string SERIES = "1111";
-        public const string NUMBER = "111111";
-        public const string ISSUED_BY = "Department of the Federal Migration Service of Russia";
-        public static readonly DateTime ISSUE_DATE = DateTime.MinValue;
-        public const string ISSUE_ID = "111-111";
-        public const string CITIZENSHIP = "Russia";
-        public const string SNILS = "111-111-111 11";
-        public const string FORM_DATA = "Example of FormData";
+        public StartRegistrationCommandHandlerTest() : base(true)
+        { }
 
         [Fact]
         [Description("Arrange RegistrationStartedEvent is called" +
                      "Act Handler for StartRegistrationCommand is called" +
                      "Assert Add new registration to database with appropriate status")]
-        public async Task Handle_RegistrationStartedEventiIsCalled_RegistrationIsAddedToDatabaseWithStatus()
+        public async Task Handle_RegistrationStartedEventIsCalled_RegistrationIsAddedToDatabaseWithStatus()
         {
             // Arrange
             var logger = new Mock<ILogger<StartRegistrationCommandHandler>>();
@@ -67,7 +57,7 @@ namespace FastRegistrator.Tests.Commands
         [Description("Arrange RegistrationStartedEvent is called" +
                      "Act Handler for StartRegistrationCommand is called" +
                      "Assert Add new registration to database with person data")]
-        public async Task Handle_RegistrationStartedEventiIsCalled_RegistrationIsAddedToDatabaseWithPersonData()
+        public async Task Handle_RegistrationStartedEventIsCalled_RegistrationIsAddedToDatabaseWithPersonData()
         {
             // Arrange
             var logger = new Mock<ILogger<StartRegistrationCommandHandler>>();
---
+++ FastRegistrator.Tests/Constants.cs
@@ -0,0 +1,19 @@
+﻿namespace FastRegistrator.Tests
+{
+    public static class Constants
+    {
+        public static readonly Guid GUID = new Guid("00000000-0000-0000-0000-000000000000");
+        public const string PERSON_PHONE_NUMBER = "+79999999999";
+        public const string FIRST_NAME = "Ivan";
+        public const string LAST_NAME = "Ivanov";
+        public const string MIDDLE_NAME = "Ivanovich";
+        public const string SERIES = "1111";
+        public const string NUMBER = "111111";
+        public const string ISSUED_BY = "Department of the Federal Migration Service of Russia";
+        public static readonly DateTime ISSUE_DATE = DateTime.MinValue;
+        public const string ISSUE_ID = "111-111";
+        public const string CITIZENSHIP = "Russia";
+        public const string SNILS = "111-111-111 11";
+        public const string FORM_DATA = "Example of FormData";
+    }
+}
