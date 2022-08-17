d98e9bd891b1d2e1fb70b67a54362de1294022b1
Danil <kr.dan1995@gmail.com>
2022-08-17 13:24:19 +0300

    Added content media type

--- FastRegistrator.Infrastructure/Services/PrizmaService.cs
+++ FastRegistrator.Infrastructure/Services/PrizmaService.cs
@@ -1,5 +1,6 @@
 ﻿using FastRegistrator.ApplicationCore.DTOs.PrizmaServiceDTOs;
 using FastRegistrator.ApplicationCore.Interfaces;
+using System.Text;
 using System.Text.Json;
 
 namespace FastRegistrator.Infrastructure.Services;
@@ -8,6 +9,8 @@ public class PrizmaService : IPrizmaService
 {
     private readonly HttpClient _httpClient;
     private const string PERSON_CHECK_PATH = "PersonCheck";
+    private const string MEDIA_TYPE = "application/json";
+
     public PrizmaService(HttpClient httpClient)
     {
         _httpClient = httpClient;
@@ -16,7 +19,8 @@ public class PrizmaService : IPrizmaService
     public async Task<PersonCheckCommonResponse> PersonCheck(PersonCheckRequest personCheckRequest, CancellationToken cancelToken)
     {
         var request = JsonSerializer.Serialize(personCheckRequest);
-        var result = await _httpClient.PostAsync(PERSON_CHECK_PATH, new StringContent(request), cancelToken);
+        var stringContent = new StringContent(request, Encoding.UTF8, MEDIA_TYPE);
+        var result = await _httpClient.PostAsync(PERSON_CHECK_PATH, stringContent, cancelToken);
         var content = await result.Content.ReadAsStringAsync(cancelToken);
 
         var personCheckCommonResponse = new PersonCheckCommonResponse();
