702a3dab130c1eaf71a0ab48a58966317d78e821
Alex Brykov <abrykov@smartru.com>
2022-08-23 12:03:01 +0300

    Added responses info for swagger

--- FastRegistrator.API/Controllers/RegistrationController.cs
+++ FastRegistrator.API/Controllers/RegistrationController.cs
@@ -3,12 +3,16 @@ using FastRegistrator.ApplicationCore.Commands.StartRegistration;
 using FastRegistrator.ApplicationCore.DTOs.RegistrationStatusDTOs;
 using FastRegistrator.ApplicationCore.Queries;
 using Microsoft.AspNetCore.Mvc;
+using System.Net;
 
 namespace FastRegistrator.API.Controllers
 {
     public class RegistrationController : ApiControllerBase
     {
         [HttpGet("{guid}")]
+        [ProducesResponseType(typeof(RegistrationStatusResponse), (int)HttpStatusCode.OK)]
+        [ProducesResponseType(typeof(ValidationProblemDetails), (int)HttpStatusCode.BadRequest)]
+        [ProducesResponseType(typeof(ProblemDetails), (int)HttpStatusCode.NotFound)]
         public async Task<ActionResult<RegistrationStatusResponse>> GetStatus(Guid guid, CancellationToken cancel)
         {
             var getStatusQuery = new GetRegistrationStatusQuery(guid);
@@ -16,6 +20,8 @@ namespace FastRegistrator.API.Controllers
         }
 
         [HttpPost("start")]
+        [ProducesResponseType((int)HttpStatusCode.OK)]
+        [ProducesResponseType(typeof(ValidationProblemDetails), (int)HttpStatusCode.BadRequest)]
         public async Task<ActionResult> StartRegistration(StartRegistrationCommand command, CancellationToken cancel)
         {
             await ExecuteCommand(command, cancel);
@@ -23,6 +29,9 @@ namespace FastRegistrator.API.Controllers
         }
 
         [HttpPost("complete")]
+        [ProducesResponseType((int)HttpStatusCode.OK)]
+        [ProducesResponseType(typeof(ValidationProblemDetails), (int)HttpStatusCode.BadRequest)]
+        [ProducesResponseType(typeof(ProblemDetails), (int)HttpStatusCode.NotFound)]
         public async Task<ActionResult> CompleteRegistration(AccountOpenedCommand command, CancellationToken cancel)
         {
             await ExecuteCommand(command, cancel);
