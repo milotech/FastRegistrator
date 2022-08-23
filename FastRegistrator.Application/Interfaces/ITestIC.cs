9d7f86d23adc78569ca1886af1bc03f53ea89eaf
Danil <kr.dan1995@gmail.com>
2022-08-23 18:02:16 +0300

    Removed redundant test IC service.

--- FastRegistrator.Application/Interfaces/ITestIC.cs
+++
@@ -1,9 +0,0 @@
-﻿using FastRegistrator.ApplicationCore.DTOs.ICRegistrationDTOs;
-
-namespace FastRegistrator.ApplicationCore.Interfaces
-{
-    public interface ITestIC
-    {
-        public Task<HttpResponseMessage> SendDataAsync(ICRegistrationData registrationData, CancellationToken cancellationToken);
-    }
-}
