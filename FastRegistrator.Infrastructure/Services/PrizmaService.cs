46432aba02dbd5617f0a352a21c0c118a5966646
EynsherKiel <eynsherkiel@gmail.com>
2022-10-19 13:47:50 +0300

    Update PrizmaService.cs

--- FastRegistrator.Infrastructure/Services/PrizmaService.cs
+++ FastRegistrator.Infrastructure/Services/PrizmaService.cs
@@ -1,5 +1,6 @@
-﻿using FastRegistrator.Application.DTOs.PrizmaService;
+using FastRegistrator.Application.DTOs.PrizmaService;
 using FastRegistrator.Application.Interfaces;
+using Microsoft.Extensions.Logging;
 using System.Text.Json;
 
 namespace FastRegistrator.Infrastructure.Services;
@@ -12,11 +13,15 @@ public class PrizmaService : IPrizmaService
     };
 
     private readonly HttpClient _httpClient;
+    private readonly ILogger<PrizmaService> _logger;
     private const string PERSON_CHECK_PATH = "PersonCheck";
 
-    public PrizmaService(HttpClient httpClient)
+    public PrizmaService(
+        HttpClient httpClient,
+        ILogger<PrizmaService> logger)
     {
         _httpClient = httpClient;
+        _logger = logger;
     }
 
     public async Task<PersonCheckResponse> PersonCheck(PersonCheckRequest personCheckRequest, CancellationToken cancelToken)
@@ -30,6 +35,9 @@ public class PrizmaService : IPrizmaService
         {
             requestUri += $"&DateOfBirth={personCheckRequest.DateOfBirth.Value:yyyy-MM-dd}";
         }
+
+        _logger.LogInformation(requestUri);
+
         var result = await _httpClient.GetAsync(requestUri, cancelToken);
         var content = await result.Content.ReadAsStringAsync(cancelToken);
 
@@ -37,6 +45,7 @@ public class PrizmaService : IPrizmaService
 
         if (result.IsSuccessStatusCode)
         {
+            _logger.LogInformation(content);
             var model = JsonSerializer.Deserialize<PersonCheckResult>(content, _jsonOptions);
             personCheckResponse.PersonCheckResult = model;
         }
@@ -44,6 +53,7 @@ public class PrizmaService : IPrizmaService
         {
             if (string.IsNullOrEmpty(content))
             {
+                _logger.LogError(content);
                 result.EnsureSuccessStatusCode();
             }
             var model = JsonSerializer.Deserialize<PersonCheckError>(content!, _jsonOptions);
